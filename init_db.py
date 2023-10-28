769431ea2b69f53cbad46a6ff9a123e5c63d2d95
pierre.auzon <pierre.auzon@gmail.com>
2023-10-28 18:17:55 +0200

    use user input to display exercise specifics

--- init_db.py
+++ init_db.py
@@ -12,7 +12,7 @@ data = {
     "theme": ["cross_joins", "window_functions"],
     "exercise_name": ["beverages_and_food", "simple_window"],
     "tables": [["beverages", "food_items"], "simple_window"],
-    "last_reviewed": ["1970-01-01", "1970-01-01"]
+    "last_reviewed": ["1970-01-01", "1970-01-01"],
 }
 memory_state_df = pd.DataFrame(data)
 con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
