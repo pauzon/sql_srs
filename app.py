36440b2a0199842577d4e9424f9e2e5eff898d55
pierre.auzon <pierre.auzon@gmail.com>
2023-10-29 17:46:36 +0100

    last touches

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import logging
 import duckdb
 import streamlit as st
+from datetime import date, timedelta
 
 
 if "data" not in os.listdir():
@@ -31,6 +32,9 @@ def check_users_solution(user_query: str) -> None:
     try:
         result = result[solution_df.columns]
         st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
     except KeyError as e:
         st.write("Some columns are missing")
     n_lines_difference = result.shape[0] - solution_df.shape[0]
@@ -68,13 +72,27 @@ with st.sidebar:
     solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
-query = st.text_area(label="votre code SQL ici", key="user_input")
+form = st.form("my_form")
+query = form.text_area(label="votre code SQL ici", key="user_input")
+form.form_submit_button("Submit")
 
 if query:
     check_users_solution(query)
 
-tab2, tab3 = st.tabs(["Tables", "Solution"])
+for n_days in [2, 7, 21]:
+    if st.button(f"Revoir dans {n_days} jours"):
+        next_review = date.today() + timedelta(days=n_days)
+        con.execute(
+            f"UPDATE memory_state SET last_reviewed = '{next_review}' WHERE exercise_name = '{exercise_name}'"
+        )
+        st.rerun()
 
+if st.button("Reset"):
+    con.execute(f"UPDATE memory_state SET last_reviewed = '1970-01-01'")
+    st.rerun()
+
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 with tab2:
     exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
